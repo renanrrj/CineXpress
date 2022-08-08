a9fcd4dc5dddb9bbf42a62036203671e4db4e847
David Renan da Silva Ramos <renanrrj@icloud.com>
2022-08-08 10:22:03 -0300

    teste

--- CineXpress.Model/obj/Release/net6.0/CineXpress.Model.AssemblyInfo.cs
+++ CineXpress.Model/obj/Release/net6.0/CineXpress.Model.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("CineXpress.View")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("CineXpress.Model")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("CineXpress.View")]
-[assembly: System.Reflection.AssemblyTitleAttribute("CineXpress.View")]
+[assembly: System.Reflection.AssemblyProductAttribute("CineXpress.Model")]
+[assembly: System.Reflection.AssemblyTitleAttribute("CineXpress.Model")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
